f0de57020434383713b8d8ac541a17a916270730
adlonymous <adlonymous@gmail.com>
2025-05-29 22:21:19 -0400

    added constant in chunk

--- chunk.h
+++ chunk.h
@@ -2,7 +2,10 @@
 #define cmouslang_chunk_h
 
 #include "common.h"
+#include "value.h"
+
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -10,9 +13,12 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
+    ValueArray* constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+int addConstant(Chunk* chunk, Value value);
 #endif
